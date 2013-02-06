1a9f1ff665f22c3a53f8d67a8f624b60f17ed80e
Robson F. Sales <robson.fsales@gmail.com>
2013-02-06 15:46:45 -0200

    Primeiro commit. Teste de Fibonacci de 0 e sua implementacao

---
+++ src/main/java/br/com/app/Fibonacci.java
@@ -0,0 +1,8 @@
+package br.com.app;
+
+public class Fibonacci {
+
+	public int calcularFibonacci(int numero){
+		return 0;
+	}
+}
---
+++ src/test/java/br/com/teste/TesteFibonacci.java
@@ -0,0 +1,16 @@
+package br.com.teste;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.app.Fibonacci;
+
+
+public class TesteFibonacci {
+
+	@Test
+	public void testFibonacci(){
+		Fibonacci fibonacci = new Fibonacci();
+		assertEquals(0, fibonacci.calcularFibonacci(0));
+	}
+}
