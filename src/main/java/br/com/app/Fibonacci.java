1f84c3b859b02c3b4b5bf99647d23babd64d013a
Robson F. Sales <robson.fsales@gmail.com>
2013-02-06 19:46:51 -0200

    Fibonacci e testes finalizados com sucesso

--- src/main/java/br/com/app/Fibonacci.java
+++ src/main/java/br/com/app/Fibonacci.java
@@ -3,7 +3,10 @@ package br.com.app;
 public class Fibonacci {
 
 	public int calcularFibonacci(int numero){
-		if(numero == 0) return 0;
-		return 1;
+		if (numero == 0)
+			return 0;
+		if (numero <= 2)
+			return 1;
+		return calcularFibonacci(numero - 1) + calcularFibonacci(numero - 2);
 	}
 }
--- src/test/java/br/com/teste/TesteFibonacci.java
+++ src/test/java/br/com/teste/TesteFibonacci.java
@@ -11,7 +11,7 @@ public class TesteFibonacci {
 	@Test
 	public void testFibonacci(){
 		Fibonacci fibonacci = new Fibonacci();
-		int [][] casosTeste = {{0,0},{1,1},{2,1}};
+		int [][] casosTeste = {{0,0},{1,1},{2,1},{3,2},{4,3},{5,5},{6,8},{7,13},{8,21}};
 
 		for(int i = 0; i < casosTeste.length; i++)
 			assertEquals(casosTeste[i][1], fibonacci.calcularFibonacci(casosTeste[i][0]));
